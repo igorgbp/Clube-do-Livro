af11bc2eb3ec78791eae4f853977b2d09ae545bf
igorgbp <igorgabriel12bq@gmail.com>
2022-12-12 12:49:40 -0300

    asdf

--- js/admin_script.js
+++ js/admin_script.js
@@ -1,10 +1,7 @@
 let navbar = document.querySelector('.header .flex .navbar');
 let userBox = document.querySelector('.header .flex .account-box');
 
-document.querySelector('#menu-btn').onclick = () =>{
-   navbar.classList.toggle('active');
-   userBox.classList.remove('active');
-}
+
 
 document.querySelector('#user-btn').onclick = () =>{
    userBox.classList.toggle('active'); 
